832da9379a4123687848de0dd4dbaeeca737553b
VanPhuHien <phien0689@gmail.com>
2024-11-15 19:12:46 +0700

    Update function.py
    Update function.py

--- function.py
+++ function.py
@@ -3,12 +3,13 @@ import pandas as pd
 
 def Search(x):
     global list_search
+    global c
+    
     list_search = []
-
     c = input()
 
     for i in x:
-        if c in i.lower():
+        if c.lower() in i.lower():
             list_search.append(i)
 
     if len(list_search) == 0:
