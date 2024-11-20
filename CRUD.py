334e6430498dac0edf8edf7a14c42fc6d758a8c4
VanPhuHien <phien0689@gmail.com>
2024-11-20 11:37:55 +0700

    NEW Update CRUD.py
    FULL

--- CRUD.py
+++ CRUD.py
@@ -2,22 +2,22 @@
 # 1. CREATE a new row in the data set
 def Create(my_df):
     new_app = {}
-    new_app["id"] = int(input("Enter the app ID: "))
+    new_app["id"] = input("Enter the app ID: ")
     new_app["track_name"] = input("Enter the app name: ")
-    new_app["size_bytes"] = int(input("Enter the app size (in bytes): "))
+    new_app["size_bytes"] = input("Enter the app size (in bytes): ")
     new_app["currency"] = input("Enter the currency: ")
-    new_app["price"] = float(input("Enter the app price: "))
-    new_app["rating_count_tot"] = int(input("Enter the total rating count: "))
-    new_app["rating_count_ver"] = int(input("Enter the version rating count: "))
-    new_app["user_rating"] = float(input("Enter the user rating: "))
-    new_app["user_rating_ver"] = float(input("Enter the user rating for the version: "))
+    new_app["price"] = input("Enter the app price: ")
+    new_app["rating_count_tot"] = input("Enter the total rating count: ")
+    new_app["rating_count_ver"] = input("Enter the version rating count: ")
+    new_app["user_rating"] = input("Enter the user rating: ")
+    new_app["user_rating_ver"] = input("Enter the user rating for the version: ")
     new_app["ver"] = input("Enter the version: ")
     new_app["cont_rating"] = input("Enter the content rating: ")
     new_app["prime_genre"] = input("Enter the primary genre: ")
-    new_app["sup_devices.num"] = int(input("Enter the number of supported devices: "))
-    new_app["ipadSc_urls.num"] = int(input("Enter the number of iPad screenshots: "))
-    new_app["lang.num"] = int(input("Enter the number of languages: "))
-    new_app["vpp_lic"] = int(input("Enter the VPP license (0 or 1): "))
+    new_app["sup_devices.num"] = input("Enter the number of supported devices: ")
+    new_app["ipadSc_urls.num"] = input("Enter the number of iPad screenshots: ")
+    new_app["lang.num"] = input("Enter the number of languages: ")
+    new_app["vpp_lic"] = input("Enter the VPP license (0 or 1): ")
 
     # create DataFrame from new application and merge into current DataFrame
     new_app_df = pd.DataFrame(new_app, index=[0])
