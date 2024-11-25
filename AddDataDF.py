9d9bba33c79edd9951d92629b4a62ce75e31c31b
Hoa <hoaqazae@gmail.com>
2024-11-25 16:42:10 +0700

    Update AddDataDF.py

--- AddDataDF.py
+++ AddDataDF.py
@@ -6,26 +6,36 @@ my_df = pd.read_csv(file_path)
 print("Initial dataset:")
 print(my_df.head(10))
 
+# function to check input data type and request re-entry if incorrect
+def get_valid_input(prompt, expected_type, allowed_values=None):
+    while True:
+        user_input = input(prompt)
+        try:
+            value = expected_type(user_input)
+            if allowed_values and value not in allowed_values:
+                print(f"Error: Value must be one of {allowed_values}. Please try again")
+                continue
+            return value
+        except ValueError:
+            print(f"Error: Invalid input. Expected a value of type {expected_type.__name__}. Please try again")
 
 # 1. CREATE a new row in the data set
 def add_new_row(my_df, file_path):
-    new_app = {}
-    new_app['id'] = int(input("Enter the app ID: "))
-    new_app['track_name'] = input("Enter the app name: ")
-    new_app['size_bytes'] = int(input("Enter the app size (in bytes): "))
-    new_app['currency'] = input("Enter the currency: ")
-    new_app['price'] = float(input("Enter the app price: "))
-    new_app['rating_count_tot'] = int(input("Enter the total rating count: "))
-    new_app['rating_count_ver'] = int(input("Enter the version rating count: "))
-    new_app['user_rating'] = float(input("Enter the user rating: "))
-    new_app['user_rating_ver'] = float(input("Enter the user rating for the version: "))
-    new_app['ver'] = input("Enter the version: ")
-    new_app['cont_rating'] = input("Enter the content rating: ")
-    new_app['prime_genre'] = input("Enter the primary genre: ")
-    new_app['sup_devices.num'] = int(input("Enter the number of supported devices: "))
-    new_app['ipadSc_urls.num'] = int(input("Enter the number of iPad screenshots: "))
-    new_app['lang.num'] = int(input("Enter the number of languages: "))
-    new_app['vpp_lic'] = int(input("Enter the VPP license (0 or 1): "))
+    print("\nEnter the details for the new app:")
+    new_app = {
+        'id': get_valid_input("Enter the app ID (integer): ", int),
+        'track_name': input("Enter the app name: "),
+        'size_bytes': get_valid_input("Enter the app size (in bytes, integer): ", int),
+        'currency': input("Enter the currency: "),
+        'price': get_valid_input("Enter the app price (float): ", float),
+        'rating_count_tot': get_valid_input("Enter the total rating count (integer): ", int),
+        'user_rating': get_valid_input("Enter the user rating (float, 0.0 - 5.0): ", float),
+        'user_rating_ver': get_valid_input("Enter the user rating for the version (float, 0.0 - 5.0): ", float),
+        'ver': input("Enter the version: "),
+        'cont_rating': input("Enter the content rating: "),
+        'prime_genre': input("Enter the primary genre: "),
+        'sup_devices.num': get_valid_input("Enter the number of supported devices (integer): ", int)
+    }
 
     # create DataFrame from new application and merge into current DataFrame
     new_app_df = pd.DataFrame(new_app, index=[0])
@@ -36,12 +46,11 @@ def add_new_row(my_df, file_path):
     print(f"Dataset updated and saved to {file_path}")
     return updated_df
 
-
 # 2. read data from the my_dfset
 def read_data(my_df):
     print()
     print("Reading dataset:")
-    print(my_df.head(100))
+    print(my_df.head(10))
     print(my_df.describe())
 
 my_df = add_new_row(my_df, file_path)
