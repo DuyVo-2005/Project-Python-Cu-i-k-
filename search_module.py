1190bff7aac4ecda0218da97c910490781bdc8e1
VanPhuHien <phien0689@gmail.com>
2024-11-26 14:25:32 +0700

    Update search_module.py

--- search_module.py
+++ search_module.py
@@ -1,12 +1,7 @@
 import pandas as pd
 
 
-def Search(my_df, x):
-    global c
-
-    list_search = []
-    c = input()
-    
+def Search(my_df, x, c):
     if x == "id":
         return filtered_data = my_df[my_df[x].astype(str) == c]
     
