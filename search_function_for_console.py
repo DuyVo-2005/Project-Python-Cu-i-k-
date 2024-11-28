eddf6fe04522b6b144e1bfe417315bc1b7af93da
VanPhuHien <phien0689@gmail.com>
2024-11-28 16:56:40 +0700

    Update search_function_for_console.py

--- search_function_for_console.py
+++ search_function_for_console.py
@@ -14,29 +14,27 @@ def Search(my_df):
     print("Enter 0 to exit")
     choice = int(input("Select the type of information you want to search for: "))
 
-    if choice < 0 and choice > 4:
-        print("ERROR!")
-                
     if choice == 0:
         return
             
-    print("Enter 0 to exit")
-            
-    if choice == 1:
+    elif choice == 1:
         print("Enter the ID of the app: ", end="")
         x = "id"
             
-    if choice == 2:
+    elif choice == 2:
         print("Enter the Name of the app: ", end="")
         x = "track_name"
                 
-    if choice == 3:
+    elif choice == 3:
         print("Enter the Version of the app: ", end="")
         x = "ver"
                 
-    if choice == 4:
+    elif choice == 4:
         print("Enter the Primary Genre of the app: ", end="")
         x = "prime_genre"
+
+    else:
+        print("ERROR!")
 
     list_search = []
     c = input()
@@ -51,3 +49,13 @@ def Search(my_df):
     if not list_search:
         print("Not found!")
     return list_search
+
+
+def console(my_df):
+    while True:
+        Search(my_df)
+        if choice == 0:
+            break
+        else:
+            for i in list_search:
+                print(i)
